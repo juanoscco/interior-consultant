bdbbc305d0af2482f9c2f978341d8cb275a697a1
Juan Oscco <juan.osccomori@gmail.com>
2023-08-20 01:55:45 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-list");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+// document.querySelectorAll(".nav-list").forEach((n) => {
+// n.addEventListener("click", () => {
+//     burger.classList.remove("active");
+//     nav.classList.remove("active");
+// });
+// });
